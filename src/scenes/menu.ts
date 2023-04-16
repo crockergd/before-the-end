135e12cb367cc999fd6d6cad08186737f61ee19e
George Crocker <crockergd@outlook.com>
2023-04-16 00:27:14 -0400

    Moved text on menu screen.

--- src/scenes/menu.ts
+++ src/scenes/menu.ts
@@ -15,7 +15,12 @@ export default class Menu extends AbstractScene {
     public create(): void {
         this.render_context.transition_scene(TransitionType.IN);
 
-        const text: AbstractText = this.render_context.add_text(this.render_context.space_buffer, this.render_context.space_buffer, 'Menu' + Constants.LINE_BREAK + 'Click to begin');
+        const title: AbstractText = this.render_context.add_text(this.render_context.center_x, this.render_context.center_y, 'Before the End');
+        title.set_anchor(0.5, 0.5);
+        title.set_scale(5, 5);
+
+        const text: AbstractText = this.render_context.add_text(title.x, title.y + this.render_context.literal(50), 'Click to begin');
+        text.set_anchor(0.5, 0.5);
 
         this.input.once(Constants.UP_EVENT, () => {
             this.render_context.transition_scene(TransitionType.OUT, new CallbackBinding(() => {
